65f59cf8c24617eb3771dae1562b7fc6e5b36f4c
aaaz425 <101464713+aaaz425@users.noreply.github.com>
2023-02-06 15:24:52 +0900

    [level 0] Title: 분수의 덧셈, Time: 8.80 ms, Memory: 36.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -0,0 +1,18 @@
+function solution(numer1, denom1, numer2, denom2) {
+    let answer = [];
+    let denom = denom1 * denom2;
+    const num1 = numer1 * denom2;
+    const num2 = numer2 * denom1;
+    let numer = num1 + num2;
+    for (let i = 1000 ** 2; i > 2; i--) {
+        if(denom % i === 0 && numer % i === 0) {
+            denom = denom / i;
+            numer = numer / i;
+        } else {
+            continue;
+        }
+    }
+    answer.push(numer);
+    answer.push(denom);
+    return answer;
+}
