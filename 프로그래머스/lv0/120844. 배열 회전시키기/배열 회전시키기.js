5a0244a2882aff7c9a416271ea4c781c0bbe34d0
aaaz425 <101464713+aaaz425@users.noreply.github.com>
2023-02-19 17:02:25 +0900

    Update 배열 회전시키기.js

--- 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -1,13 +1,4 @@
 function solution(numbers, direction) {
-    let number = 0;
-    if(direction === 'left') {
-        number = numbers[0];
-        numbers.shift();
-        numbers.push(number);
-    } else {
-        number = numbers[numbers.length-1];
-        numbers.pop();
-        numbers.unshift(number);
-    }
+    direction === 'right' ? numbers.unshift(numbers.pop()) : numbers.push(numbers.shift());
     return numbers;
 }
