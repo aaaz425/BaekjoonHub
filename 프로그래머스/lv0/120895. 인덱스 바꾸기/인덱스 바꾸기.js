115d2c6ecf3265389042188b84ab6598fe9fe28e
aaaz425 <101464713+aaaz425@users.noreply.github.com>
2023-02-20 18:00:45 +0900

    Update 인덱스 바꾸기.js

--- 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -4,3 +4,10 @@ function solution(my_string, num1, num2) {
     answer[num2] = my_string[num1];
     return answer.join('')
 }
+
+// 구조 분해 할당 활용
+function solution(my_string, num1, num2) {
+    my_string = my_string.split('');
+    [my_string[num1], my_string[num2]] = [my_string[num2], my_string[num1]];
+    return my_string.join('');
+}
