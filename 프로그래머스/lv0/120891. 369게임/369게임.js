e8758b5beb63d9af5f553e4720ea277943a82b2d
aaaz425 <101464713+aaaz425@users.noreply.github.com>
2023-02-22 11:12:02 +0900

    Update 369게임.js

--- 프로그래머스/lv0/120891. 369게임/369게임.js
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -3,3 +3,9 @@ function solution(order) {
         .filter(v => v == 3 || v == 6 || v == 9)
         .length;
 }
+
+// 정규표현식, matchAll 사용
+function solution(order) {
+    var answer = [...order.toString().matchAll(/[3|6|9]/g)].length;
+    return answer;
+}
