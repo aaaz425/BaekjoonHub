042bf1006f01e365f2e207cabd036032ff5c0b06
aaaz425 <101464713+aaaz425@users.noreply.github.com>
2023-02-25 23:37:35 +0900

    Update 팩토리얼.js

--- 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.js
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.js
@@ -1,21 +1,6 @@
 function solution(n) {
-  let start = 1;
-  let end = n;
-  while (start <= end) {
-    const mid = Math.floor((start + end) / 2);
-    if (factorial(mid) <= n) {
-      start = mid + 1;
-    } else {
-      end = mid - 1;
-    }
-  }
-  return end;
-}
-
-function factorial(n) {
-  let result = 1;
-  for (let i = 1; i <= n; i++) {
-    result *= i;
-  }
-  return result;
+    let i = 1;
+    let f = 1;
+    while (f*i < n) f*=++i;
+    return i;
 }
