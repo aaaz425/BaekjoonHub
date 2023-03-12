1338dd368fa21322cfdcbc92addc0d09c27d62eb
TOKO <101464713+aaaz425@users.noreply.github.com>
2023-03-13 02:41:10 +0900

    Update 두 정수 사이의 합.js

--- 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -1,3 +1,4 @@
+// 시간복잡도 꽝
 function solution(a, b) {
     if(a >= b) {
         return Array.from({length : a - b + 1}, (_, i) => i + b)
@@ -6,4 +7,9 @@ function solution(a, b) {
         return Array.from({length : b - a + 1}, (_, i) => i + a)
         .reduce((acc, cur) => acc + cur);
     }
+}
+
+// 참고)) 가우스 덧셈 공식
+function solution(a, b) {
+    return (a + b) * (Math.abs(a - b) + 1) / 2;
 }
