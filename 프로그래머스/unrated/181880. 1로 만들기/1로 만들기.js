49336e2953f62e189dc51deaa9c99970f691d9d6
TOKO <101464713+aaaz425@users.noreply.github.com>
2023-05-26 11:25:00 +0900

    Update 1로 만들기.js

--- 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.js
+++ 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.js
@@ -1,17 +1,12 @@
 function solution(num_list) {
-    const answer = [];
+    let count = 0;
     
-    for(let i = 0; i < num_list.length; i++) {
-        let value = num_list[i];
-        let count = 0;
-        
+    num_list.forEach(value => {
         while(value !== 1) {
             value = value % 2 === 1 ? (value - 1) / 2 : value / 2;
             count++;
         }
-        
-        answer.push(count)    
-    }
+    })
     
-    return answer.reduce((acc, cur) => acc + cur, 0);
+    return count;
 }
