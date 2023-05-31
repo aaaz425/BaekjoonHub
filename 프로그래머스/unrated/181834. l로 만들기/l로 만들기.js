1fb43be5d22f9a33e23c5fdf8c291467dc55ba52
TOKO <101464713+aaaz425@users.noreply.github.com>
2023-05-31 16:00:15 +0900

    [unrated] Title: l로 만들기, Time: 7.06 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/unrated/181834. l로 만들기/l로 만들기.js
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.js
@@ -1,17 +1,5 @@
-function solution(myString) {
-    const letters = [
-        'a','b','c',
-        'd','e','f',
-        'g','h','i',
-        'j','k','l',
-        'm','n','o',
-        'p','q','r',
-        's','t','u',
-        'v','w','x',
-        'y','z'
-    ];
-    
+function solution(myString) {   
     return [...myString].map(value => {
-        return letters.indexOf(value) < letters.indexOf('l') ? 'l' : value;
+        return value < 'l' ? 'l' : value;
     }).join('')
 }
