3e9e6a38cb6be4360be5c5c80221764a6ad0f3f5
TOKO <101464713+aaaz425@users.noreply.github.com>
2023-06-04 00:33:59 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
@@ -1,5 +1,5 @@
 function solution(a, b, c) {
-    let newSet = [...new Set([a, b, c])];
+    const newSet = [...new Set([a, b, c])];
     switch(newSet.length) {
         case 1:
             return (a + b + c) * (a ** 2 + b ** 2 + c ** 2) * (a ** 3 + b ** 3 + c ** 3);
