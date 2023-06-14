dbeebb8fdac6a0dae845672911d7749125b0038f
TOKO <101464713+aaaz425@users.noreply.github.com>
2023-06-14 18:05:02 +0900

    [unrated] Title: 간단한 논리 연산, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.js
+++ 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.js
@@ -1,6 +1,3 @@
-function solution(x1, x2, x3, x4) {
-    const left = x1 || x2 ? true : false;
-    const right = x3 || x4 ? true: false;
-    
-    return left && right ? true: false;
+function solution(x1, x2, x3, x4) {  
+    return (x1 || x2) && (x3 || x4) ? true: false;
 }
