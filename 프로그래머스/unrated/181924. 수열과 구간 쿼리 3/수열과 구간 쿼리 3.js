1a9e2802a6c4f95c1e2ac183ba033944963de94c
TOKO <101464713+aaaz425@users.noreply.github.com>
2023-06-26 09:43:24 +0900

    [unrated] Title: 수열과 구간 쿼리 3, Time: 0.29 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
@@ -1,8 +1,6 @@
 function solution(arr, queries) {
     queries.forEach(query => {
-        const temp = arr[query[0]];
-        arr[query[0]] = arr[query[1]];
-        arr[query[1]] = temp;
+        [arr[query[0]], arr[query[1]]] = [arr[query[1]], arr[query[0]]];
     })
     
     return arr;
