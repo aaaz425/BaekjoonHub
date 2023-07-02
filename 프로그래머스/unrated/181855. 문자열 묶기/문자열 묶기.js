a7c9a288ed3b8aca6f2d5213febdc7c6a32915fe
TOKO <101464713+aaaz425@users.noreply.github.com>
2023-07-03 02:18:27 +0900

    [unrated] Title: 문자열 묶기, Time: 43.51 ms, Memory: 54.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.js
+++ 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.js
@@ -1,11 +1,14 @@
 function solution(strArr) {
-    const group = [];
-    
     const lengArr = strArr.map(str => {
         return str.length;
     });
     
     const setArr = [...new Set(lengArr)];
     
-    return Math.max(...setArr.map((key, idx) => lengArr.filter(value => value === setArr[idx]).length));
+    const answerArr = setArr
+        .map((key, idx) => {
+            return lengArr.filter(value => value === setArr[idx]).length
+        })
+    
+    return Math.max(...answerArr);
 }
