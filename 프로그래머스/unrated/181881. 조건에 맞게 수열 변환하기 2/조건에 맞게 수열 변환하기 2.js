1a6f4c5b4eba69b1767240159ce0d81a6a91475c
TOKO <101464713+aaaz425@users.noreply.github.com>
2023-07-06 21:01:13 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 2, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.js
@@ -0,0 +1,27 @@
+function solution(arr) {
+    let count = 0;
+    let isSameArr = false;
+    
+    let baseArr = [...arr];
+    let newArr = [];
+    
+    while(!isSameArr) {
+        newArr = baseArr.map(value => {
+            if (value % 2 === 0 && value >= 50) {
+                return value / 2;   
+            } else if (value % 2 === 1 && value < 50) {
+                return value * 2 + 1;
+            } else {
+                return value;
+            }
+        });
+        
+        if (baseArr.join('') == newArr.join('')) {
+            isSameArr = true;
+            return count;
+        }
+        
+        baseArr = [...newArr];
+        count++;
+    }
+}
