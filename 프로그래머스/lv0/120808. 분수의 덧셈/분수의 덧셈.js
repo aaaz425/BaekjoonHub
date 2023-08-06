dc92e1cd786617cd0b087ac4589ffb909a0c4a34
TOKO <101464713+aaaz425@users.noreply.github.com>
2023-08-06 23:41:34 +0900

    Update 분수의 덧셈.js

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,9 +1,11 @@
 function solution(numer1, denom1, numer2, denom2) {
     let answer = [];
+    
     let denom = denom1 * denom2;
     const num1 = numer1 * denom2;
     const num2 = numer2 * denom1;
     let numer = num1 + num2;
+    
     for (let i = 1000 ** 2; i > 2; i--) {
         if(denom % i === 0 && numer % i === 0) {
             denom = denom / i;
@@ -12,7 +14,9 @@ function solution(numer1, denom1, numer2, denom2) {
             continue;
         }
     }
+    
     answer.push(numer);
     answer.push(denom);
+    
     return answer;
 }
