9d012476e90ad4a8ca54f4377f4f4bda55cd2641
TOKO <101464713+aaaz425@users.noreply.github.com>
2023-08-19 09:26:14 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.js
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.js
@@ -1,7 +1,3 @@
 function solution(my_string) {
-    var answer = '';
-    const splitStr = my_string.split('');
-    const reverseStr = splitStr.reverse();
-    answer = reverseStr.join('');
-    return answer;
+    return [...my_string].reverse().join('')
 }
