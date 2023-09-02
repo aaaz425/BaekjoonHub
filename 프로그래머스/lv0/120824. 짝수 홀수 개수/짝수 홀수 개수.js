77709c31312f6a79c091a9d7f49358c9c38c5716
TOKO <101464713+aaaz425@users.noreply.github.com>
2023-09-02 23:30:08 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,9 +1,4 @@
 function solution(num_list) {
-    var answer = [];
-    let odd = 0;
-    let even = 0;
-    num_list.forEach(element => element % 2 === 0 ? even++ : odd++);
-    answer.push(even);
-    answer.push(odd);
-    return answer;
+    const oddCount = num_list.filter(num => num % 2 === 1).length;
+    return [num_list.length - oddCount, oddCount];
 }
