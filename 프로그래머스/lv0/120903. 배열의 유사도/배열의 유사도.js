0502c461631ed1996585380453f0477ece830654
TOKO <101464713+aaaz425@users.noreply.github.com>
2023-09-10 21:35:04 +0900

    [level 0] Title: 배열의 유사도, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,5 +1,5 @@
 function solution(s1, s2) {
-    var answer = 0;
-    s1.forEach(element => s2.includes(element) ? answer++ : answer);
-    return answer;
+    let count = 0;
+    s1.forEach(element => s2.includes(element) ? count++ : count);
+    return count;
 }
