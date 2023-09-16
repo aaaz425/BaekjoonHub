55acf0c2cca64ec810e37356d0ad4d79f13a3524
TOKO <101464713+aaaz425@users.noreply.github.com>
2023-09-17 00:05:39 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -1,4 +1,3 @@
 function solution(my_string) {
-    const my_arr = [...my_string]
-    return my_arr.reduce((acc, cur) => +cur * 0 === 0 ? acc + +cur : acc, 0)
+    return [...my_string].reduce((acc, cur) => +cur * 0 === 0 ? acc + +cur : acc, 0)
 }
