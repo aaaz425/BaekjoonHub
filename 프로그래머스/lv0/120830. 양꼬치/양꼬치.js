0f158194839d4b649ec4fdbcbace6f17703c81e7
TOKO <101464713+aaaz425@users.noreply.github.com>
2023-09-27 22:24:56 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
@@ -1,9 +1,3 @@
 function solution(n, k) {
-    // var answer = 0;
-    // answer += n * 12000;
-    // answer += k * 2000;
-    // answer += -(Math.floor(n/10) * 2000);
-    // return answer;
-    
     return 12000 * n + 2000 * k - (Math.floor(n/10) * 2000)
 }
