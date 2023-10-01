65c77b407d37426e55f6f16c9a8113ac5669d1b1
TOKO <101464713+aaaz425@users.noreply.github.com>
2023-10-01 14:34:03 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.js
@@ -1,8 +1,3 @@
 function solution(n) {
-    var answer = 0;
-    const arr = String(n).split('').map((num)=>{
-      return Number(num)
-    });
-    answer = arr.reduce((acc, cur) => acc + cur, 0);
-    return answer;
+    return n.toString().split('').reduce((acc, cur) => acc + +cur, 0);
 }
