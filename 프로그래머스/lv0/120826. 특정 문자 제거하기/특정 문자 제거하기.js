2e0d103c6f4583987e7ae0f13963e68a98655aa0
TOKO <101464713+aaaz425@users.noreply.github.com>
2023-10-01 14:37:29 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -1,7 +1,3 @@
 function solution(my_string, letter) {
-    var answer = '';
-    const my_array = my_string.split('');
-    const filter = my_array.filter(element => element !== letter)
-    answer = filter.join('');
-    return answer;
+    return my_string.replaceAll(letter, "");
 }
