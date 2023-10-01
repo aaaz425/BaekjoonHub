cdfda8a39773a710ff8fdc8be4e9b1e8314047f2
TOKO <101464713+aaaz425@users.noreply.github.com>
2023-10-01 14:49:50 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.js
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.js
@@ -1,1 +1,3 @@
-const solution = (array) => array.sort((a,b) => a-b)[Math.floor(array.length / 2)]
+function solution(array) {
+    return array.sort((a, b) => a - b)[Math.floor(array.length / 2)];
+}
