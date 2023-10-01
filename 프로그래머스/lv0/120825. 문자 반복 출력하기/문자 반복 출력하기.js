85eebfaefd628838ab8843ca65f5ee9833683eb0
TOKO <101464713+aaaz425@users.noreply.github.com>
2023-10-01 14:45:31 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
+++ 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -1,5 +1,3 @@
 function solution(my_string, n) {
-    var answer = [...my_string].map(v => v.repeat(n)).join("");
-    console.log(answer);
-    return answer;
+    return [...my_string].map(v => v.repeat(n)).join("");
 }
