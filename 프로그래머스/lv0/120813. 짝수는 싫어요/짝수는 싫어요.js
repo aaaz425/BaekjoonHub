0400baead013a2de39a7e9115d4450a1289bc4b3
TOKO <101464713+aaaz425@users.noreply.github.com>
2023-10-01 14:52:38 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -1,5 +1,4 @@
 function solution(n) {
     return Array.from({length: n}, (_, i) => i + 1)
-    .filter(value => value % 2 === 1)
-    .sort((a, b) => a - b);
+    .filter(value => value % 2 === 1);
 }
