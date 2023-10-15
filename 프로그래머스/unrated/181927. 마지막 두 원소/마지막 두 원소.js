5e47dc33e4b64a0ce52b4770b9fac3200747e513
TOKO <101464713+aaaz425@users.noreply.github.com>
2023-10-15 20:39:13 +0900

    [level unrated] Title: 마지막 두 원소, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.js
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.js
@@ -1,11 +1,4 @@
 function solution(num_list) {
-    const arrLen = num_list.length;
-    const lastIndex = arrLen - 1;
-    const beforeLastIndex = arrLen - 2;
-    if (num_list[lastIndex] > num_list[beforeLastIndex]) {
-        num_list.push(num_list[lastIndex] - num_list[beforeLastIndex]);
-    } else {
-        num_list.push(num_list[lastIndex] * 2);
-    }
-    return num_list;
+    const lastNum = num_list.at(-1) > num_list.at(-2) ? num_list.at(-1) - num_list.at(-2) : num_list.at(-1) * 2;
+    return [...num_list, lastNum];
 }
