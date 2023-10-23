50cc14848f629aea7fabde70e6f2b0f862aafcad
TOKO <101464713+aaaz425@users.noreply.github.com>
2023-10-23 22:59:21 +0900

    [level unrated] Title: 홀수 vs 짝수, Time: 0.15 ms, Memory: 32.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.js
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -1,10 +1,10 @@
 function solution(num_list) {
-    let odd = 0;
-    let even = 0;
+    const oddSum = num_list
+        .filter((_, i) => i % 2 === 0)
+        .reduce((acc, cur) => acc + +cur, 0);
+    const evenSum = num_list
+        .filter((_, i) => i % 2 === 1)
+        .reduce((acc, cur) => acc + +cur, 0);
     
-    for (let i = 0; i < num_list.length; i++) {
-        i % 2 === 1 ? odd += num_list[i] : even += num_list[i];
-    }
-
-    return odd >= even ? odd : even;
+    return evenSum >= oddSum ? evenSum : oddSum;
 }
