f3ae79b8fecba3a86ebb67ece72e255356986f8e
TOKO <101464713+aaaz425@users.noreply.github.com>
2023-11-18 23:52:47 +0900

    [level unrated] Title: 홀짝 구분하기, Time: 47.49 ms, Memory: 32.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181944. 홀짝 구분하기/홀짝 구분하기.js
+++ 프로그래머스/unrated/181944. 홀짝 구분하기/홀짝 구분하기.js
@@ -9,8 +9,8 @@ let input = [];
 rl.on('line', function (line) {
     input = line.split(' ');
 }).on('close', function () {
-    n = Number(input[0]);
-    console.log(n % 2 === 0 ? 
+    const [n] = input;
+    console.log(+n % 2 === 0 ? 
                 `${n} is even` :
                 `${n} is odd`)
 });
