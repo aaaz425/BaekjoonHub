b2b2fac97175f718e3db4d9f72b5916291b1bf0f
TOKO <101464713+aaaz425@users.noreply.github.com>
2023-11-20 17:50:58 +0900

    [level unrated] Title: 전국 대회 선발 고사, Time: 0.29 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
@@ -0,0 +1,13 @@
+function solution(rank, attendance) {
+    const ranking = rank.map((score, index) => {
+        return {
+            score: score,
+            index: index
+        }
+    })
+    const attendableRank = ranking.filter((_, index) => attendance[index]);
+    const sortedRank = attendableRank.sort((a, b) => a.score - b.score);
+    const [a, b, c] = sortedRank.slice(0, 3);
+    
+    return (10000 * a.index) + (100 * b.index) + c.index;
+}
