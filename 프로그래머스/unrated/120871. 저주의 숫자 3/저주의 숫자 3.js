051e3394633261003c50cb00b6ad0cc51bde8f7c
TOKO <101464713+aaaz425@users.noreply.github.com>
2023-11-25 00:07:35 +0900

    [level unrated] Title: 저주의 숫자 3, Time: 0.62 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -0,0 +1,16 @@
+function solution(n) {
+    const thousandsArr = Array.from({length: 1000}, (_, i) => i + 1);
+    const filteredArr = thousandsArr.filter((num) => {
+        if (num % 3 === 0) {
+            return false;
+        }
+        
+        if (num.toString().includes('3')) {
+            return false;
+        }
+        
+        return true;
+    })
+          
+    return filteredArr[n - 1];
+}
