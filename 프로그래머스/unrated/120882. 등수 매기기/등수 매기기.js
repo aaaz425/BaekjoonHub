79ee9d40ca44221d87e1b37e4c8fcb11a098b45b
TOKO <101464713+aaaz425@users.noreply.github.com>
2023-11-25 00:39:37 +0900

    [level unrated] Title: 등수 매기기, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120882. 등수 매기기/등수 매기기.js
+++ 프로그래머스/unrated/120882. 등수 매기기/등수 매기기.js
@@ -2,5 +2,5 @@ function solution(score) {
     const average = score.map((value) => value.reduce((acc, cur) => acc + cur, 0) / 2);
     const sortedAverage = [...average].sort((a, b) => b - a);
 
-    return average.map(score => sortedAverage.findIndex((rankedScore) => rankedScore === score) + 1);
+    return average.map(score => sortedAverage.indexOf(score) + 1);
 }
