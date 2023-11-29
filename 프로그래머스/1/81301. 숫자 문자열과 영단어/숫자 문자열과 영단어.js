5fef91b73b0d441ef9593346eb925a8e79f57211
TOKO <101464713+aaaz425@users.noreply.github.com>
2023-11-29 17:23:06 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,10 +1,9 @@
 function solution(s) {
-    let answer = s;
     const charOfNum = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
     
     charOfNum.forEach((char, index) => {
-        answer = answer.replaceAll(char, index);
+        s = s.replaceAll(char, index);
     })
     
-    return +answer;
+    return +s;
 }
