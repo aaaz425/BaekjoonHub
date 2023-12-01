d702bebbe3b5f1ca077820d4f7b5179e66ca89e7
TOKO <101464713+aaaz425@users.noreply.github.com>
2023-12-01 23:14:59 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.87 ms, Memory: 35.6 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -1,13 +1,13 @@
 function solution(numbers) {
-    const answer = [];
+    const set = new Set();
     
     numbers.forEach((number1, index1) => {
         numbers.forEach((number2, index2) => {
             if (index1 !== index2) {
-                answer.push(number1 + number2);
+                set.add(number1 + number2);
             }
         })
     })
     
-    return [...new Set(answer)].sort((a, b) => a- b);
+    return [...set].sort((a, b) => a- b);
 }
