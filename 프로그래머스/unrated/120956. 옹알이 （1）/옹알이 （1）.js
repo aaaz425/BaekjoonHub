2751232ce19e549ba3889a30028a16243357ed0d
TOKO <101464713+aaaz425@users.noreply.github.com>
2023-12-04 17:41:23 +0900

    [level unrated] Title: 옹알이 (1), Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120956. 옹알이 （1）/옹알이 （1）.js
+++ 프로그래머스/unrated/120956. 옹알이 （1）/옹알이 （1）.js
@@ -1,31 +1,12 @@
 function solution(babbling) {
-    const possible = ["aya", "ye", "woo", "ma"];
-    
-    return babbling.reduce((acc, cur) => {
-        let findCount = 0;
-        let replaceCount = 0;
-        
-        possible.forEach(word => {
-            if (cur.includes(word)) {
-                findCount += 1;
-            }
-        })
-        
-        possible.forEach(word => {
-            if (cur.includes(word)) {
-                cur = cur.replace(word, "");
-                replaceCount += 1;    
-            }
-        })
-        
-        if (replaceCount !== findCount) {
-            return acc;
-        }
-        
-        if (!cur.length) {
-            return acc + 1;
-        }
-            
-        return acc;
-    }, 0);
+  let answer = 0;
+  const regex = /^(aya|ye|woo|ma)+$/;
+
+  babbling.forEach(word => {
+    if (regex.test(word)) {
+        answer++; 
+    } 
+  })
+
+  return answer;
 }
