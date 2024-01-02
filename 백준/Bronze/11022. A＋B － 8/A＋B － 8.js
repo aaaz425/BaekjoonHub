836b04f99ba879ce2f8a3f96118d9c20d9da3a14
TOKO <101464713+aaaz425@users.noreply.github.com>
2024-01-02 12:36:43 +0900

    [Bronze V] Title: A+B - 8, Time: 152 ms, Memory: 9680 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.js
@@ -0,0 +1,26 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const input = [];
+
+rl.on('line', (line) => {
+  input.push(line);
+
+  if (!line) {
+    rl.close();
+  }
+}).on('close', () => {
+  const [count, ...rest] = input;
+
+  rest.forEach((value, index) => {
+    const [a, b] = value.split(' ');
+
+    console.log(`Case #${index + 1}: ${a} + ${b} = ${+a + +b}`);
+  });
+
+  process.exit();
+});
