c6038e4bf4e89e976e63e90a2dd96db5e5517364
TOKO <101464713+aaaz425@users.noreply.github.com>
2024-01-09 22:19:08 +0900

    [Bronze V] Title: 문자와 문자열, Time: 144 ms, Memory: 9436 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.js
@@ -0,0 +1,22 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const input = [];
+
+rl.on('line', (line) => {
+  input.push(line);
+
+  if (!line) {
+    rl.close();
+  }
+}).on('close', () => {
+  const [word, index] = input;
+
+  console.log(word[index - 1]);
+
+  process.exit();
+});
