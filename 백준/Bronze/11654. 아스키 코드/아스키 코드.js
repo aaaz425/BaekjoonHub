b98ec8b59bec062e1977e955fadceac4147b495a
TOKO <101464713+aaaz425@users.noreply.github.com>
2024-01-12 03:04:16 +0900

    [Bronze V] Title: 아스키 코드, Time: 152 ms, Memory: 9456 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.js
@@ -0,0 +1,16 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on('line', (line) => {
+  console.log(line.charCodeAt(0));
+
+  if (!line) {
+    rl.close();
+  }
+}).on('close', () => {
+  process.exit();
+});
