87a5492b4d76e4656e87b84112137666f8d8cea1
TOKO <101464713+aaaz425@users.noreply.github.com>
2024-03-18 23:08:07 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 9.80 ms, Memory: 36.1 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -1,9 +1,7 @@
 function solution(strings, n) {
-    const char = 'abcdefghijklmnopqrstuvwxyz';
-    
     return strings.sort((a, b) => {
         if (a[n] !== b[n]) {
-            return char.indexOf(a[n]) - char.indexOf(b[n])    
+            return a[n].localeCompare(b[n]);
         }
         
         return a.localeCompare(b);
