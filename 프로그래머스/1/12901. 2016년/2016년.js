56f17546fa449be1c27692948abdd9821f8c52cc
TOKO <101464713+aaaz425@users.noreply.github.com>
2024-03-22 13:51:30 +0900

    [level 1] Title: 2016년, Time: 0.11 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.js
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -1,6 +1,6 @@
 function solution(a, b) {
     const day = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
-    const date = new Date(`2016-${a.length === 1 ? `0${a}` : a}-${b.length === 1 ? `0${b}` : b}`)
+    const date = new Date(`2016-${a}-${b}`)
     
     return day[date.getDay()];
 }
