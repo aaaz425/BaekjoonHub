677a9f14bfb67fd85f96b75c7902943a6579ed15
TOKO <101464713+aaaz425@users.noreply.github.com>
2024-04-22 19:55:35 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 160 ms, Memory: 9632 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.js
@@ -0,0 +1,16 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on('line', (line) => {
+  console.log(line);
+
+  if (!line && line !== '') {
+    rl.close();
+  }
+}).on('close', () => {
+  process.exit();
+});
