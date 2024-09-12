e0ae89bdbdbf0198f6b8790577b66cb5fc98bc42
aaaz425 <ytokogg@gmail.com>
2024-09-12 10:46:10 +0900

    [level 3] Title: 정수 삼각형, Time: 4.62 ms, Memory: 42 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -1,4 +1,24 @@
 function solution(triangle) {
-    var answer = 0;
-    return answer;
+  for (let i = 1; i < triangle.length; i++) {
+    for (let j = 0; j < triangle[i].length; j++) {
+      let left = 0;
+      let right = 0;
+
+      if (j !== 0) {
+        left = triangle[i - 1][j - 1];
+      }
+
+      if (j !== triangle[i].length - 1) {
+        right = triangle[i - 1][j];
+      }
+
+      if (left >= right) {
+        triangle[i][j] += left;
+      } else {
+        triangle[i][j] += right;
+      }
+    }
+  }
+
+  return Math.max(...triangle.at(-1));
 }
