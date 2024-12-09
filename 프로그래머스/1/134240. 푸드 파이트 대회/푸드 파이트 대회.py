3f9421962dd0bc5da92e199364b92cfa883c45a0
TOKO <101464713+aaaz425@users.noreply.github.com>
2024-12-09 14:27:32 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 1.54 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,12 +1,10 @@
 def solution(food):
-    answer = []
     water, *real_food = food
+    answer = [0]
     
     for idx, val in enumerate(reversed(real_food)):
         count = val // 2
         value = len(real_food) - idx
         answer = [value] * count + answer + [value] * count
-
-    answer.insert(len(answer) // 2, 0)
     
     return ''.join(map(str, answer))
