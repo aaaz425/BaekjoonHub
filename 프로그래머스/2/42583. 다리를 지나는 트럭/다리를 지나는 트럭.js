7e29d4a7e9f10d25ce6b896a5bb01e97df29bf68
TOKO <101464713+aaaz425@users.noreply.github.com>
2025-08-16 12:58:10 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 29.08 ms, Memory: 37.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -0,0 +1,23 @@
+function solution(bridge_length, weight, truck_weights) {
+    let time = 0;
+    let bridge = Array(bridge_length).fill(0);
+    let totalWeight = 0;
+  
+    while (truck_weights.length > 0 || totalWeight > 0) {
+      time++;
+
+      totalWeight -= bridge.shift();
+
+      if (truck_weights.length > 0) {
+        if (totalWeight + truck_weights[0] <= weight) {
+          let truck = truck_weights.shift();
+          bridge.push(truck);
+          totalWeight += truck;
+        } else {
+          bridge.push(0);
+        }
+      }
+    }
+
+    return time;
+}
