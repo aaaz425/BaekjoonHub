25c40303426d98bc2f0d280fd15916fbf4d9c2b6
TOKO <101464713+aaaz425@users.noreply.github.com>
2025-10-18 15:50:30 +0900

    [Bronze V] Title: Multiply, Time: 124 ms, Memory: 9612 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/22193. Multiply/Multiply.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const A = BigInt(input[1]);
+const B = BigInt(input[2]);
+
+console.log((A * B).toString());
