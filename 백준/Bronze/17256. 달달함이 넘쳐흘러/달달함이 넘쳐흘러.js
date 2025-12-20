1f6d65c11e5e02cd38b126737c82eaab46b96258
TOKO <101464713+aaaz425@users.noreply.github.com>
2025-12-20 17:59:28 +0900

    [Bronze IV] Title: 달달함이 넘쳐흘러, Time: 92 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/17256. 달달함이 넘쳐흘러/달달함이 넘쳐흘러.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim().split('\n');
+
+const [ax, ay, az] = input[0].split(' ').map(Number);
+const [cx, cy, cz] = input[1].split(' ').map(Number);
+
+const bx = cx - az;
+const by = cy / ay;
+const bz = cz - ax;
+
+console.log(bx, by, bz);
