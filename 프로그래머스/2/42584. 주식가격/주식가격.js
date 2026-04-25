69a373687d0d947b7f477e6e5609580d36d61eda
aaaz425 <101464713+aaaz425@users.noreply.github.com>
2026-04-25 23:52:32 +0900

    [level 2] Title: 주식가격, Time: 29.36 ms, Memory: 41.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,14 +1,19 @@
 function solution(prices) {
-    return prices.map((val, idx) => {
-        let sum = 0;
-        for (let i = idx + 1; i < prices.length; i++) {
-            sum += 1;
-            
-            if (val > prices[i]) {
-                break;
-            }
-        }
-        
-        return sum;
-    })
+  const answer = Array(prices.length).fill(0);
+  const stack = [];
+  
+  for (let i = 0; i < prices.length; i++) {
+    while (stack.length && prices[i] < prices[stack[stack.length - 1]]) {
+      const prev = stack.pop();
+      answer[prev] = i - prev;
+    }
+    stack.push(i);
+  }
+  
+  while (stack.length) {
+    const prev = stack.pop();
+    answer[prev] = prices.length - 1 - prev;
+  }
+  
+  return answer;
 }
