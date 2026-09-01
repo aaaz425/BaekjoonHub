07b1590690cd0b93fa748ea5fb576da688d0f8e2
aaaz425 <101464713+aaaz425@users.noreply.github.com>
2026-09-01 14:41:40 +0900

    [level 2] Title: 전화번호 목록, Time: 239.12 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,8 +1,6 @@
 function solution(phone_book) {
-    const set = new Set();
-    for (const phone of phone_book) {
-        set.add(phone);
-    }
+    const set = new Set(phone_book);
+    
     for (const phone of phone_book) {
         for (let i = 0; i < phone.length; i++) {
             if (set.has(phone.slice(0, i))) {
