834dd4c90c644e05d14eeb64e1f1ec04f4132eb2
aaaz425 <101464713+aaaz425@users.noreply.github.com>
2026-09-02 21:58:09 +0900

    [level 2] Title: 의상, Time: 0.15 ms, Memory: 44.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,14 +1,18 @@
 function solution(clothes) {
     const map = new Map();
     
-    for (const [_, thing] of clothes) {
-        map.set(thing, (map.get(thing) || 0) + 1);
+    for (const clothing of clothes) {
+        const [_, type] = clothing;
+        if (map.has(type)) {
+            map.set(type, map.get(type) + 1);
+        } else {
+            map.set(type, 1);
+        }
     }
     
     let answer = 1;
-    
-    for (value of map.values()) {
-        answer *= value + 1;
+    for (const [key, val] of map) {
+        answer *= (val + 1);
     }
     
     return answer - 1;
